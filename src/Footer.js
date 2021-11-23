cb00cf3b725726d277acb8ce325a5df524c52d3e
Jaideep2912 <jaideep2912@gmail.com>
2021-11-23 22:32:31 +0530

    Footer

---
+++ src/Footer.js
@@ -0,0 +1,22 @@
+import React from 'react'
+import "./footer.css"
+function Footer() {
+    return (
+      <div className = "Footer">
+          <div className="top">
+              Back to top
+              </div>
+              <div className = "center">
+                      <div className="left">
+                      <div className="heading">Get to Know Us</div>
+                      About Us
+                      Careers
+                      Press Releases
+                      Amazon Cares
+                      Gift a Smile
+                      </div>
+              </div> 
+          </div>
+
+      )}
+      export default Footer
