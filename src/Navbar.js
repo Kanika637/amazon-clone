2f9fa3b3710fe2996e940e65ccb47d8522bba108
yuvrajsingh chouhan <85758285+uvsingh2706@users.noreply.github.com>
2022-09-30 12:12:55 +0530

    Update Navbar.js

--- src/Navbar.js
+++ src/Navbar.js
@@ -5,8 +5,10 @@ function Navbar() {
     <div>
       <nav
         class="navbar navbar-black "
-        style={{ backgroundColor: "#353232", color: "white" }}
+        style={{ backgroundColor: "#222f3e", color: "white" }}
       >
+         <div className="navbar__complete">
+         <div className="nav__left">
         <button
           class="navbar-toggler toggler-example"
           type="button"
@@ -21,24 +23,36 @@ function Navbar() {
             <i class="fas fa-bars fa-1x"></i>
           </span>
         </button>
-        <a class="navbar-brand white-text mr-auto" href="#" style={{fontSize:"13px", fontWeight:"bold"}}>
-          All
+       
+     
+       <a class="navbar-brand white-text mr-auto " href="#" style={{fontSize:"13px", fontWeight:"bold"}}>
+       All
         </a>
-        <a class="navbar-brand white-text mr-auto" href="#" style={{fontSize:"13px", marginLeft:"-740px"}}>
-          Today's Deal
+        <a class="navbar-brand white-text mr-auto" href="#" style={{fontSize:"13px",  fontWeight:"bold"}}>
+       Fresh
         </a>
-        <a class="navbar-brand white-text mr-auto" href="#" style={{fontSize:"13px",  marginLeft:"-760px"}}>
-          Buy Again
+        <a class="navbar-brand white-text mr-auto" href="#" style={{fontSize:"13px",  fontWeight:"bold"}}>
+        Buy Again
+
         </a>
-        <a class="navbar-brand white-text mr-auto" href="#" style={{fontSize:"13px",  marginLeft:"-760px"}}>
-          Customer services
+        <a class="navbar-brand white-text mr-auto" href="#" style={{fontSize:"13px",  fontWeight:"bold"}}>
+          Amazon Pay
         </a>
-        <a class="navbar-brand white-text mr-auto" href="#" style={{fontSize:"13px",  marginLeft:"-760px"}}>
-          Gift Cards
+        <a class="navbar-brand white-text mr-auto" href="#" style={{fontSize:"13px",  fontWeight:"bold" }}>
+         Gift cards
         </a>
-        <a class="navbar-brand white-text mr-auto" href="#" style={{fontSize:"13px",  marginLeft:"-750px"}}>
-          Sell
+        <a class="navbar-brand white-text mr-auto" href="#" style={{fontSize:"13px",  fontWeight:"bold"}}>
+        Gift Ideas
         </a>
+       </div>
+       <div className="navbar__right">
+       <img className="navbar__img" src="https://play-lh.googleusercontent.com/Av2HpYJCl4SLe3oYmEZJcnFZYwjDcDpbdtqp0pu8lad1JI_sgMIIIQHyM-HodjZQi4Qx" alt="" />
+          <span style={{fontSize:"18px"}}>Shopping made easy | Download the app</span>
+       </div>
+        </div>
+      
+      
+    
 
         <div class="collapse navbar-collapse" id="navbarSupportedContent1">
           <ul class="navbar-nav mr-auto">
