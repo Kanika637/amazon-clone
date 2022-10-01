0062a241b97028dc73d69f631d5051d0fea1f48b
Souvik-Nayak <nayaksouvik786@gmail.com>
2022-10-02 00:24:34 +0530

    initial changes Checkout Page

--- src/CheckoutProduct.js
+++ src/CheckoutProduct.js
@@ -1,49 +1,41 @@
+/* eslint-disable no-unused-vars */
 import React from 'react'
 import "./CheckoutProduct.css";
-import {useStateValue} from './StateProvider';
+import { useStateValue } from './StateProvider';
 
-
-function CheckoutProduct({id,image,title,price,rating,reviews}) {
-    const[{basket},dispatch]=useStateValue();
-
-    const removeFromBasket=()=>{
+function CheckoutProduct({ id, image, title, price, rating, reviews }) {
+    const [{ basket }, dispatch] = useStateValue();
+    const removeFromBasket = () => {
         dispatch({
-            type:'REMOVE_FROM_BASKET',
-            id:id,
+            type: 'REMOVE_FROM_BASKET',
+            id: id,
         })
     }
-       
-    
+
     return (
-        
         <div className="checkoutProduct">
             <img className="checkoutimage__product" src={image} alt="Checkout Product" />
-<div className="checkoutProduct__info">
-    <p className="checkoutProduct__title">{title}</p>
-    <p className="checkoutproduct__price">
-        <small>Rs</small>
-        <strong>{price}</strong>
-    </p>
-    <div className="checkoutproduct__rating">
-        
-         
-        
-        {Array(rating)
-        .fill()
-        .map((_,i)=>(
-            <p>⭐</p>
-        ))}
-        <span className='product__reviews'>({reviews})</span>
-    </div>
-    <button className="checkoutproduct__button" onClick={removeFromBasket}>Remove from basket</button>
+            <div className="checkoutProduct__info">
+                <div className='checkout_Alignment'>
+                    <div className="checkoutProduct__title">{title}</div>
+                    <div className="checkoutproduct__price">
+                        <small>Rs </small>
+                        <strong>{price}</strong>
+                    </div>
+                </div>
 
-</div>
-             
+                <div className="checkoutproduct__rating">
+                    {Array(rating)
+                        .fill()
+                        .map((_, i) => (
+                            <p>⭐</p>
+                        ))}
+                    <span className='product__reviews'>({reviews})</span>
+                </div>
+                <button className="checkoutproduct__button" onClick={removeFromBasket}>Remove from basket</button>
+            </div>
         </div>
-        
     )
 }
 
 export default CheckoutProduct
-
- 
