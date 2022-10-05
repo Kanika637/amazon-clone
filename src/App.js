3ca9150594e31983628e92096c16be10fdef7cb3
Shubham Patil <shubhampatil0723@gmail.com>
2022-10-06 01:32:23 +0530

    Added responsiveness for desktop and tablets

--- src/App.js
+++ src/App.js
@@ -1,97 +1,84 @@
-import './App.css';
-import React,{useEffect} from 'react';
-import Header from './Header';
-import Home from './Home';
-import {BrowserRouter as Router ,Switch,Route}
-from "react-router-dom";
+import "./App.css";
+import React, { useEffect } from "react";
+import Header from "./Header";
+import Home from "./Home";
+import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
 
-import Checkout from './Checkout'
-import Login from "./Login"; 
-import {auth} from "./firebase"; 
-import {useStateValue} from './StateProvider'
+import Checkout from "./Checkout";
+import Login from "./Login";
+import { auth } from "./firebase";
+import { useStateValue } from "./StateProvider";
 import Payment from "./Payment";
-import {Elements} from '@stripe/react-stripe-js';
-import {loadStripe} from '@stripe/stripe-js';
-import Thanku from './Thanku';
-import Navbar from './Navbar';
+import { Elements } from "@stripe/react-stripe-js";
+import { loadStripe } from "@stripe/stripe-js";
+import Thanku from "./Thanku";
+import Navbar from "./Navbar";
 
-import Footer from './Footer';
+import Footer from "./Footer";
 
-
-
-
-const stripePromise=loadStripe('pk_test_51JdCsbSDjgMnau9ncKpDOaddNIWtdhVTTV92V4ShkTzLec033vWcRQjqEUByb1s4D6vmPmH6oMK0bkBJyBlRsStp00wQV1pNuX');
+const stripePromise = loadStripe(
+  "pk_test_51JdCsbSDjgMnau9ncKpDOaddNIWtdhVTTV92V4ShkTzLec033vWcRQjqEUByb1s4D6vmPmH6oMK0bkBJyBlRsStp00wQV1pNuX"
+);
 function App() {
-const[{},dispatch]=useStateValue();
+  const [{}, dispatch] = useStateValue();
   //to keep a check who is signed in/making a listner
-  
-  useEffect(()=>{
+
+  useEffect(() => {
     //will run only once when the app component loads
-    auth.onAuthStateChanged(authUser=>{
-      console.log('USER IS >>>>',authUser);
-      if(authUser){
+    auth.onAuthStateChanged((authUser) => {
+      if (authUser) {
         //the user logged in/the user was logged in
-        {/*when someone will logg in it will shoot
+        {
+          /*when someone will logg in it will shoot
           the user ibto the data layer and vice versa if we 
-          logged out*/}
+          logged out*/
+        }
         dispatch({
-          
-          type: 'SET_USER',
-          user:authUser
-        })
-      }
-      else{
+          type: "SET_USER",
+          user: authUser,
+        });
+      } else {
         //the user is logged out
         dispatch({
-          type:'SET_USER',
-          user:null
-        })
+          type: "SET_USER",
+          user: null,
+        });
       }
-    })
-
-
-  },[])
+    });
+  }, []);
   return (
     <>
-   
-    <Router>
-    <div className="App">
-
-      <Switch>
-      
-      <Route path="/thanku">
-        <Header/>
-      
-          <Thanku/>
-          </Route>
-        <Route path="/Payment">
-          <Header/>
-          {/* wraps the payment elements,
+      <Router>
+        <div className="App">
+          <Switch>
+            <Route path="/thanku">
+              <Header />
+              <Thanku />
+            </Route>
+            <Route path="/Payment">
+              <Header />
+              {/* wraps the payment elements,
           no need to understand it */}
-          <Elements stripe={stripePromise}>
-      <Payment/>
-    </Elements>
-          
-        
-          
-          </Route>
-        <Route path="/login">
-          <Login/>
-          </Route>
-        <Route path="/checkout">
-         <Header/> 
-          <Checkout/>
-          </Route>
-<Route path="/">
-      <Header/>
-      <Navbar />
-      <Home/>
-   
-      </Route>
-      </Switch>
-      <Footer/>
-    </div>
-    </Router>
+              <Elements stripe={stripePromise}>
+                <Payment />
+              </Elements>
+            </Route>
+            <Route path="/login">
+              <Login />
+            </Route>
+            <Route path="/checkout">
+              <Header />
+              <Checkout />
+            </Route>
+            <Route path="/">
+              <Header />
+              <Navbar />
+              <Home />
+            </Route>
+          </Switch>
+          <Footer />
+        </div>
+      </Router>
     </>
   );
 }
