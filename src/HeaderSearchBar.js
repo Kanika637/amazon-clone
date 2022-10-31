2f224c963df948770377aa761ad53a0d666579ac
Aakash <vermaaakash1000@gmail.com>
2022-10-31 18:59:33 +0530

    passing the search input value to app component

--- src/HeaderSearchBar.js
+++ src/HeaderSearchBar.js
@@ -1,57 +1,61 @@
-import { useState } from "react";
-import "./Header.css";
-import SearchIcon from "@material-ui/icons/Search";
-import ArrowDropDownIcon from "@material-ui/icons/ArrowDropDown";
+import { useState } from 'react';
+import './Header.css';
+import SearchIcon from '@material-ui/icons/Search';
+import ArrowDropDownIcon from '@material-ui/icons/ArrowDropDown';
 
 const categories = [
-  "All Departments",
-  "Audible Audiobooks",
-  "Amazon Devices",
-  "Kindle Store",
-  "Prime Video",
-  "Alexa Skills",
-  "Subscribe & Save",
-  "Digital Music",
-  "Apps & Games",
-  "Japanese Books",
-  "English Books",
-  "Music",
-  "Classical",
-  "Movies & TV",
-  "Computer & Video Games",
-  "Software",
-  "Computers & Accessories",
-  "Electronics & Cameras",
-  "Stationery and Office Products",
-  "Kitchen & Housewares",
-  "Pet Supplies",
-  "Health & Personal Care",
-  "Beauty",
-  "Food & Beverage",
-  "Baby & Maternity",
-  "Fashion",
-  "Women",
-  "Men",
-  "Kids & Baby",
-  "Clothing & Accessories",
-  "Shoes & Bags",
-  "Wrist Watches",
-  "Jewelry",
-  "Toys",
-  "Hobby",
-  "Musical Instruments",
-  "Sports",
-  "Car & Bike Products",
-  "DIY, Tools & Garden",
-  "Large Appliances",
-  "Credit Cards",
-  "Gift Cards",
-  "Industrial & Scientific",
-  "Warehouse Deals",
+  'All Departments',
+  'Audible Audiobooks',
+  'Amazon Devices',
+  'Kindle Store',
+  'Prime Video',
+  'Alexa Skills',
+  'Subscribe & Save',
+  'Digital Music',
+  'Apps & Games',
+  'Japanese Books',
+  'English Books',
+  'Music',
+  'Classical',
+  'Movies & TV',
+  'Computer & Video Games',
+  'Software',
+  'Computers & Accessories',
+  'Electronics & Cameras',
+  'Stationery and Office Products',
+  'Kitchen & Housewares',
+  'Pet Supplies',
+  'Health & Personal Care',
+  'Beauty',
+  'Food & Beverage',
+  'Baby & Maternity',
+  'Fashion',
+  'Women',
+  'Men',
+  'Kids & Baby',
+  'Clothing & Accessories',
+  'Shoes & Bags',
+  'Wrist Watches',
+  'Jewelry',
+  'Toys',
+  'Hobby',
+  'Musical Instruments',
+  'Sports',
+  'Car & Bike Products',
+  'DIY, Tools & Garden',
+  'Large Appliances',
+  'Credit Cards',
+  'Gift Cards',
+  'Industrial & Scientific',
+  'Warehouse Deals',
 ];
 
-function HeaderSearchBar() {
-  const [category, setCategory] = useState("All");
+function HeaderSearchBar(props) {
+  const [category, setCategory] = useState('All');
+
+  function handleChange(event) {
+    props.onSearch(event);
+  }
   return (
     <form className="header__searchForm ">
       <div className="header__selectInput">
@@ -66,7 +70,11 @@ function HeaderSearchBar() {
           ))}
         </select>
       </div>
-      <input className="header__searchInput" type="text" />
+      <input
+        className="header__searchInput"
+        type="search"
+        onChange={handleChange}
+      />
 
       <SearchIcon className="header__searchIcon" />
     </form>
