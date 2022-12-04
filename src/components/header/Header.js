9233aa724daa9c04058011a9973bafe9d7e51510
SDE-ADNAN <adnansdeofficial@gmail.com>
2022-12-04 13:36:24 +0530

    folder structure : added the Header css and js files in components

---
+++ src/components/header/Header.js
@@ -0,0 +1,120 @@
+import React, { useState } from 'react';
+import './Header.css';
+import logo from '../../assets/amazon-logo.png';
+import { ReactComponent as LocationSvg } from '../../assets/Location.svg';
+import { ReactComponent as CartSvg } from '../../assets/Cart.svg';
+import { ReactComponent as UserSvg } from '../../assets/User.svg';
+
+import { Link } from 'react-router-dom';
+import { useStateValue } from '../../StateProvider';
+import { auth } from '../../firebase';
+import HeaderSearchBar from './HeaderSearchBar';
+
+function Header(props) {
+  const [{ basket, user }, dispatch] = useStateValue();
+  const [screenWidth, setScreenWidth] = useState(window.innerWidth);
+  {
+    /*this will help you 
+    signout*/
+  }
+
+  // Add renderOnResize function
+  const updateWidth = () => {
+    let width = window.innerWidth;
+    setScreenWidth(width);
+  };
+
+  const debounce = (func, time) => {
+    let timer;
+    return function () {
+      let context = this,
+        args = arguments;
+      clearTimeout(timer);
+      timer = setTimeout(() => {
+        func.apply(context, args);
+      }, time);
+    };
+  };
+
+  const optimizedWidthSetter = debounce(updateWidth, 100);
+  window.addEventListener('resize', optimizedWidthSetter);
+
+  const renderNonMobileElements = () => {
+    return (
+      <>
+        <div onClick={handleAuthentication} className="header__option">
+          <span className="header__optionLineOne">Hello, {user?.name}</span>
+          {/*this will handle the authentication
+    if user is already signed in it will show 
+    signout*/}
+          <span className="header__optionLineTwo">
+            {user ? 'Sign Out' : 'Sign In'}
+          </span>
+        </div>
+        <div className="header__option">
+          <span className="header__optionLineOne">Returns</span>
+          <span className="header__optionLineTwo">& Orders</span>
+        </div>
+      </>
+    );
+  };
+
+  const renderMobileElements = () => {
+    return (
+      <>
+        <div onClick={handleAuthentication} className="header__option">
+          <UserSvg />
+          <span>{user ? user.name : 'Sign In'}</span>
+        </div>
+      </>
+    );
+  };
+
+  const handleAuthentication = () => {
+    if (user) {
+      auth.signOut();
+    }
+  };
+
+  return (
+    <div className="header">
+      <Link to="/">
+        <img className="header__logo" src={logo} alt="Amazon logo" />
+      </Link>
+      {screenWidth > 790 ? (
+        <div className="header__locationOption">
+          <span className="header__optionLineOne">Hello</span>
+          <span className="header__locationOptionLineTwo">
+            <LocationSvg /> Select your location
+          </span>
+        </div>
+      ) : (
+        ''
+      )}
+      <div className="header__search">
+        <HeaderSearchBar onSearch={props.inputHandler} />
+      </div>
+      <div className="header__nav">
+        {/*if there is no user then only go to login pg*/}
+        <Link to={!user && '/login'} className="header__right">
+          {screenWidth >= 650
+            ? renderNonMobileElements()
+            : renderMobileElements()}
+        </Link>
+        <Link to="/checkout" className="header__checkout">
+          <div className="header__optionBasket">
+            <CartSvg />
+            <div className="header__basketCount">{basket.length}</div>
+          </div>
+          {screenWidth > 860 ? (
+            <div className="header__basketText">Cart</div>
+          ) : (
+            ''
+          )}
+        </Link>
+      </div>
+    </div>
+  );
+}
+
+export default Header;
---
+++ src/components/header/HeaderSearchBar.js
@@ -0,0 +1,83 @@
+import { useState } from 'react';
+import './Header.css';
+import SearchIcon from '@material-ui/icons/Search';
+import ArrowDropDownIcon from '@material-ui/icons/ArrowDropDown';
+
+const categories = [
+  'All Departments',
+  'Audible Audiobooks',
+  'Amazon Devices',
+  'Kindle Store',
+  'Prime Video',
+  'Alexa Skills',
+  'Subscribe & Save',
+  'Digital Music',
+  'Apps & Games',
+  'Japanese Books',
+  'English Books',
+  'Music',
+  'Classical',
+  'Movies & TV',
+  'Computer & Video Games',
+  'Software',
+  'Computers & Accessories',
+  'Electronics & Cameras',
+  'Stationery and Office Products',
+  'Kitchen & Housewares',
+  'Pet Supplies',
+  'Health & Personal Care',
+  'Beauty',
+  'Food & Beverage',
+  'Baby & Maternity',
+  'Fashion',
+  'Women',
+  'Men',
+  'Kids & Baby',
+  'Clothing & Accessories',
+  'Shoes & Bags',
+  'Wrist Watches',
+  'Jewelry',
+  'Toys',
+  'Hobby',
+  'Musical Instruments',
+  'Sports',
+  'Car & Bike Products',
+  'DIY, Tools & Garden',
+  'Large Appliances',
+  'Credit Cards',
+  'Gift Cards',
+  'Industrial & Scientific',
+  'Warehouse Deals',
+];
+
+function HeaderSearchBar(props) {
+  const [category, setCategory] = useState('All');
+
+  function handleChange(event) {
+    props.onSearch(event);
+  }
+  return (
+    <form className="header__searchForm ">
+      <div className="header__selectInput">
+        <span className="header__selectInputText">{category}</span>
+        <ArrowDropDownIcon className="header__selectInputIcon" />
+        <select onChange={(e) => setCategory(e.target.value)}>
+          <option>All</option>
+          {categories.map((cat, idx) => (
+            <option key={idx} value={cat}>
+              {cat}
+            </option>
+          ))}
+        </select>
+      </div>
+      <input
+        className="header__searchInput"
+        type="search"
+        onChange={handleChange}
+      />
+
+      <SearchIcon className="header__searchIcon" />
+    </form>
+  );
+}
+export default HeaderSearchBar;
